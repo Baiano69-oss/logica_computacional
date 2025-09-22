50c8e2f5304e789de6174cafe409feffb75838aa
Baiano69-oss <Victor.conegundes.farias@escola.pr.gov.br>
2025-09-22 12:07:41 -0300

    laco-for.py

---
+++ laco-for.py
@@ -0,0 +1,6 @@
+https://www.programiz.com/online-compiler/1GwjQxrrgNvB2
+https://www.programiz.com/online-compiler/6Ii7JBXf3blsZ
+https://www.programiz.com/online-compiler/8oGw4qXV9Etjo
+https://www.programiz.com/online-compiler/50mo6pmMtUgmP
+https://www.programiz.com/online-compiler/5PO0D3yJOlSdE
+https://www.programiz.com/online-compiler/4fAFB6xNbylkm
